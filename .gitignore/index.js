8324575b3f6a7430ccae8aa6f8a39e053f031109
Sayen-Raigin <36884403+Sayen-Raigin@users.noreply.github.com>
2018-06-14 13:16:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -56,7 +56,7 @@ bot.on('message', message => {
     
     // ---------------------------------- Partie troll ----------------------------------//
     
-    var key_word1 = new RegExp("@Zenka#0373");var key_word2 = new RegExp("bisous sucrés");
+    var key_word1 = new RegExp("Zenka");var key_word2 = new RegExp("bisous sucrés");
     test = key_word1.test(message.content); var test2= key_word2.test(message.content)
     if(  (test && test2) && (message.author.username === 'Sαyen-∧lphα') ){
         message.channel.sendMessage("Oui elle est kiffe les bisous sucrés ! Hein Zenka")
